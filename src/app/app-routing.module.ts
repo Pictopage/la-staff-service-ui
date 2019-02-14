6b65062a8e1caa1460d036469432f9923c5e345a
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-14 22:36:36 +0000

    Add routerlink to Staff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StaffComponent} from './staff/staff.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'staff', component: StaffComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
