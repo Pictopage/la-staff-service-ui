359653d463ca30687bcfe142d908fa610df42dc3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-14 00:02:54 +0000

    Add Staff component

---
+++ src/app/staff/staff.ts
@@ -0,0 +1,10 @@
+export class Staff {
+  name: string;
+  surname: string;
+  address: string;
+  profileSummary: string;
+  role: string;
+  payRate: string;
+  mileage: string;
+  completeTraining: string[];
+}
