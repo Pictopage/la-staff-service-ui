03715734494712dd057e08df45f09f667d32add0
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-15 21:26:35 +0000

    Randomise data

--- src/app/ui/staff/staff.service.ts
+++ src/app/ui/staff/staff.service.ts
@@ -13,61 +13,61 @@ export class StaffService {
       address: '28 march close, Sunningdale, Surrey',
       profileSummary: 'T.Jenkins - Cool dude',
       role: 'Director',
-      payRate: '450/hr'
+      payRate: '450/day'
     },
     {
       id: 2,
-      name: 'Thomas',
-      surname: 'Jenkins',
+      name: 'Lauren',
+      surname: 'Smith',
       address: '28 march close, Sunningdale, Surrey',
-      profileSummary: 'T.Jenkins - Cool dude',
-      role: 'Director',
-      payRate: '450/hr'
+      profileSummary: 'L.Smith - 10 years exp',
+      role: 'Head Nurse',
+      payRate: '50/hr'
     },
     {
       id: 3,
-      name: 'Thomas',
-      surname: 'Jenkins',
+      name: 'Kat',
+      surname: 'Williams',
       address: '28 march close, Sunningdale, Surrey',
-      profileSummary: 'T.Jenkins - Cool dude',
-      role: 'Director',
-      payRate: '450/hr'
+      profileSummary: 'K.Williams - 5 years exp',
+      role: 'Nurse',
+      payRate: '30/hr'
     },
     {
       id: 4,
-      name: 'Thomas',
-      surname: 'Jenkins',
+      name: 'Stephen',
+      surname: 'Johns',
       address: '28 march close, Sunningdale, Surrey',
-      profileSummary: 'T.Jenkins - Cool dude',
-      role: 'Director',
-      payRate: '450/hr'
+      profileSummary: 'S.Johns - 5 months exp',
+      role: 'Trainee Nurse',
+      payRate: '0/hr'
     },
     {
       id: 5,
-      name: 'Thomas',
-      surname: 'Jenkins',
+      name: 'Lauren',
+      surname: 'Hill',
       address: '28 march close, Sunningdale, Surrey',
-      profileSummary: 'T.Jenkins - Cool dude',
-      role: 'Director',
-      payRate: '450/hr'
+      profileSummary: 'L.Hill - Been working for 6 years.',
+      role: 'Carer',
+      payRate: '25/hr'
     },
     {
       id: 6,
-      name: 'Thomas',
-      surname: 'Jenkins',
+      name: 'Obert',
+      surname: 'Roberts',
       address: '28 march close, Sunningdale, Surrey',
-      profileSummary: 'T.Jenkins - Cool dude',
-      role: 'Director',
-      payRate: '450/hr'
+      profileSummary: 'O.Roberts - Been working for 3 years',
+      role: 'Cleaner',
+      payRate: '10/hr'
     },
     {
       id: 7,
-      name: 'Thomas',
-      surname: 'Jenkins',
+      name: 'Terrence',
+      surname: 'Phillips',
       address: '28 march close, Sunningdale, Surrey',
-      profileSummary: 'T.Jenkins - Cool dude',
-      role: 'Director',
-      payRate: '450/hr'
+      profileSummary: 'T.Phillips - Been working for 16 years',
+      role: 'Cook',
+      payRate: '250/day'
     }
   ];
 
