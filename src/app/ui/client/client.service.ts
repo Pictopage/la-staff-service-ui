0f06fbeeee69af7946f5b6aaaaa6995f12e61002
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-18 08:42:12 +0000

    Add Client ID

--- src/app/ui/client/client.service.ts
+++ src/app/ui/client/client.service.ts
@@ -5,6 +5,7 @@ export class ClientService {
 
   private clientMembers: Client[] = [
     {
+      id: '12345',
       name: 'Jeremy',
       surname: 'Kyle',
       nhsNumber: '123456',
@@ -12,6 +13,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '23456',
       name: 'Sandy',
       surname: 'Jackson',
       nhsNumber: '123456',
@@ -19,6 +21,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '34567',
       name: 'Kyle',
       surname: 'Kuzma',
       nhsNumber: '123456',
@@ -26,6 +29,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '45678',
       name: 'Lebron',
       surname: 'James',
       nhsNumber: '123456',
@@ -33,6 +37,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '4123123',
       name: 'Kobe',
       surname: 'Bryant',
       nhsNumber: '123456',
@@ -40,6 +45,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '532433',
       name: 'Kemba',
       surname: 'Walker',
       nhsNumber: '123456',
@@ -47,6 +53,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '493481',
       name: 'Thomas',
       surname: 'Jefferson',
       nhsNumber: '123456',
@@ -54,6 +61,7 @@ export class ClientService {
       healthHistory: 'Head-aches, Cases of Migraines'
     },
     {
+      id: '943812',
       name: 'Paul',
       surname: 'George',
       nhsNumber: '123456',
--- src/app/ui/client/client.ts
+++ src/app/ui/client/client.ts
@@ -1,4 +1,5 @@
 export class Client {
+  id: string
   name: string;
   surname: string;
   nhsNumber: string;
