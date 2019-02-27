147de876ce448c1eba6ba91036fb5d2c6df99a6c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-27 09:50:25 +0000

    Add Sorting for Client

--- src/app/ui/clients/client/client.component.ts
+++ src/app/ui/clients/client/client.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Client} from '../client';
 import {ClientService} from '../client.service';
+import {Sort} from '@angular/material';
 
 @Component({
   selector: 'app-client',
@@ -10,6 +11,7 @@ import {ClientService} from '../client.service';
 export class ClientComponent implements OnInit {
 
   clientMembers: Client[];
+  sortedMembersData: Client[];
 
   constructor(private clientService: ClientService) { }
 
@@ -24,4 +26,43 @@ export class ClientComponent implements OnInit {
       );
   }
 
+
+  sortData(sort: Sort) {
+    const data = this.clientMembers.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedMembersData = data;
+      return;
+    }
+
+    // id: number;
+    // name: string;
+    // surname: string;
+    // nhsNumber: string;
+    // healthStatus: string;
+    // healthHistory: string;
+
+    this.sortedMembersData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id':
+          return compare(a.id, b.id, isAsc);
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'surname':
+          return compare(a.surname, b.surname, isAsc);
+        case 'nhsNumber':
+          return compare(a.nhsNumber, b.nhsNumber, isAsc);
+        case 'healthStatus':
+          return compare(a.healthStatus, b.healthStatus, isAsc);
+        case 'protein':
+          return compare(a.healthHistory, b.healthHistory, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
