4af47e0b1c043849b903632bcfb40b0f8811704b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-28 21:14:16 +0000

    Add sorting to Reports

--- src/app/ui/reports/report/report.component.ts
+++ src/app/ui/reports/report/report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Report} from '../report';
 import {ReportService} from '../report.service';
+import {Sort} from '@angular/material';
 
 @Component({
   selector: 'app-report',
@@ -10,6 +11,7 @@ import {ReportService} from '../report.service';
 export class ReportComponent implements OnInit {
 
   allReports: Report[];
+  sortedReports: Report[];
 
   constructor(private reportService: ReportService) { }
 
@@ -24,4 +26,35 @@ export class ReportComponent implements OnInit {
       );
   }
 
+  sortData(sort: Sort) {
+    const data = this.allReports.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedReports = data;
+      return;
+    }
+
+    this.sortedReports = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id':
+          return compare(a.id, b.id, isAsc);
+        case 'groupId':
+          return compare(a.groupId, b.groupId, isAsc);
+        case 'groupType':
+          return compare(a.groupType, b.groupType, isAsc);
+        case 'numberOfHours':
+          return compare(a.numberOfHours, b.numberOfHours, isAsc);
+        case 'title':
+          return compare(a.title, b.title, isAsc);
+        case 'missedCalls':
+          return compare(a.missedCalls, b.missedCalls, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
